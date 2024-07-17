9315552cc03d48f726e6a37f719fbf01b52ef6c2
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-07-17 21:38:55 +0200

    implements unit tests

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const blogsRouter = require("./controllers/blogs");
+const app = express();
+const cors = require("cors");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((err) => {
+    logger.error("error connecting to MongoDB:", err.message);
+  });
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.morganMiddle);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
